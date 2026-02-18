97f3fa7ed2b1493a756176f11982db2e4503a2a5
Weida Chen <weidac1@uci.edu>
2026-02-17 19:09:30 -0800

    Imported some materials and set up script for statemachine

---
+++ GDIM32-Final/Assets/Scripts/CharacterBaseState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBaseState
+{
+
+    public virtual void Enter()
+    {
+        
+    }
+
+    public virtual void Exit()
+    {
+        
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+
+    public virtual void FixedUpdate()
+    {
+        
+    }
+}
